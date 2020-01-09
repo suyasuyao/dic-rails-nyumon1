ad6ea3cf70c7c8cd89e2b219218e28709bb89092
yasuyasu-ot <y.otsubo.dss@gmail.com>
2020-01-10 06:42:46 +0900

    rollback実行

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_08_170659) do
+ActiveRecord::Schema.define(version: 0) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "blogs", force: :cascade do |t|
-    t.string "title"
-    t.text "content"
-  end
 
 end
