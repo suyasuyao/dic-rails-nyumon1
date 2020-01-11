614ec7f23005eae93e569c34b79fb722ecf12310
yasuyasu-ot <y.otsubo.dss@gmail.com>
2020-01-12 04:28:20 +0900

    rubocopの設定修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-#   get 'stocks/index'
-    get '/stocks', to: 'stocks#index'
-#   get 'blogs/index'
-#   get '/blogs', to: 'blogs#index' #追記する
+  #   get 'stocks/index'
+  get "/stocks", to: "stocks#index"
+  #   get 'blogs/index'
+  #   get '/blogs', to: 'blogs#index' 追記する
   resources :blogs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
