dc0557c64bfb76d73220fbc5a6e1fc507b6c0d26
yasuyasu-ot <y.otsubo.dss@gmail.com>
2020-01-12 15:17:42 +0900

    Rails入門23【バリデーションについて】 バリデーションの実装

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -9,10 +9,14 @@ class BlogsController < ApplicationController
   end
 
   def create
-    Blog.create(blog_params)
-
-    # redirect_to "/blogs/new"
-    redirect_to new_blog_path
+    @blog = Blog.new(blog_params)
+    if @blog.save
+      # 一覧画面へ遷移して"ブログを作成しました！"とメッセージを表示します。
+      redirect_to blogs_path, notice: "ブログを作成しました！"
+    else
+      # 入力フォームを再描画します。
+      render :new
+    end
   end
 
   # 定義する
