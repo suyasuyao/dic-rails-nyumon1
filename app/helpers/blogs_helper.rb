36a8b3c24fc284d53966ddc406f7f238cecc5b47
yasuyasu-ot <y.otsubo.dss@gmail.com>
2020-01-13 16:32:25 +0900

    Rails入門31【投稿確認画面をnewアクションの時にだけ出す実装について】 バリデーションを画面確認前に実施

--- app/helpers/blogs_helper.rb
+++ app/helpers/blogs_helper.rb
@@ -1,6 +1,5 @@
 module BlogsHelper
   def choose_new_or_edit
-    binding.pry
     if action_name == "new" || action_name == "confirm" || action_name == "create"
       confirm_blogs_path
     elsif action_name == "edit"
