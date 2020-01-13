25bb0d561ef941d7912b1d0abb9c5913121f79fd
yasuyasu-ot <y.otsubo.dss@gmail.com>
2020-01-13 15:55:32 +0900

    Rails入門31【投稿確認画面をnewアクションの時にだけ出す実装について】 helperメソッドの実装 投稿確認画面の実装

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
   get "/stocks", to: "stocks#index"
   #   get 'blogs/index'
   #   get '/blogs', to: 'blogs#index' 追記する
-  resources :blogs
+  resources :blogs do
+    collection do
+      post :confirm
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
